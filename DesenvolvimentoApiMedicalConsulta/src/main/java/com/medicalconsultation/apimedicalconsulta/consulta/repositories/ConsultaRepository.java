10b217bebd6fd529b8e907360c3ca8de5baa8067
Arsidney <128491841+Arsidney@users.noreply.github.com>
2024-11-12 11:40:47 -0300

    Add files via upload

---
+++ DesenvolvimentoApiMedicalConsulta/src/main/java/com/medicalconsultation/apimedicalconsulta/consulta/repositories/ConsultaRepository.java
@@ -0,0 +1,9 @@
+package com.medicalconsultation.apimedicalconsulta.consulta.repositories;
+
+import com.medicalconsultation.apimedicalconsulta.consulta.domain.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+}
---
+++ DesenvolvimentoApiMedicalConsulta/src/main/java/com/medicalconsultation/apimedicalconsulta/consulta/services/ConsultaService.java
@@ -0,0 +1,60 @@
+package com.medicalconsultation.apimedicalconsulta.consulta.services;
+
+
+import com.medicalconsultation.apimedicalconsulta.consulta.domain.Consulta;
+import com.medicalconsultation.apimedicalconsulta.consulta.repositories.ConsultaRepository;
+import com.medicalconsultation.apimedicalconsulta.exceptions.ExceptionDataIntegrityViolation;
+import lombok.AllArgsConstructor;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class ConsultaService {
+
+
+    private ConsultaRepository consultaRepository;
+
+
+    public Consulta cadastrarConsulta(Consulta consulta){
+        consulta.setIdConsulta(null);
+        return consultaRepository.save(consulta);
+    }
+
+    public List<Consulta> listarConsultas() {
+        return consultaRepository.findAll();
+    }
+
+    public Consulta buscarConsulta(Long id) {
+        Optional<Consulta> consulta = consultaRepository.findById((id));
+        return consulta.orElseThrow(() -> new ObjectNotFoundException("Consulta não encontrado! ID: ", id));
+    }
+
+    public void deletarConsulta(Long id){
+        buscarConsulta(id);
+        try{
+            consultaRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e){
+            throw new ExceptionDataIntegrityViolation("Não é possível excluir, porque há entidades relacionadas.") ;
+        }
+    }
+
+    public Consulta atualizarConsulta(Consulta consulta) {
+        Consulta novaConsulta = buscarConsulta(consulta.getIdConsulta());
+        updateData(novaConsulta, consulta);
+        return consultaRepository.save(novaConsulta);
+    }
+//atualizar os dados da consulta
+    private void updateData(Consulta novaConsulta, Consulta consulta){
+        novaConsulta.setDataConsulta(consulta.getDataConsulta());
+        novaConsulta.setProfissional(consulta.getProfissional());
+        novaConsulta.setEspecialidade(consulta.getEspecialidade());
+    }
+}
+
+
+
