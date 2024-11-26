61ba6a9dd9d14ca839d39a08771194b484b94f1b
Arsidney <128491841+Arsidney@users.noreply.github.com>
2024-11-26 17:06:45 -0300

    Delete DesenvolvimentoApiMedicalConsulta directory

--- DesenvolvimentoApiMedicalConsulta/src/main/java/com/medicalconsultation/apimedicalconsulta/consulta/repositories/ConsultaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicalconsultation.apimedicalconsulta.consulta.repositories;
-
-import com.medicalconsultation.apimedicalconsulta.consulta.domain.Consulta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
-}
--- DesenvolvimentoApiMedicalConsulta/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO USUARIOS (NOME_USUARIO, EMAIL, CPF, TELEFONE, DATANASCIMENTO, PERMISSAO)
-VALUES ('ArsidneyMartins', 'arsidney@gmail.com', '10020035000', '83988889999', '1985-10-15', 'ADMIN'),
-('Fulano', 'fulano@gmail.com', '22233355545', '83988558855', '1991-02-02', 'SECRETARIO'),
-('Cicrano', 'cidrano@gmail.com', '11133355544', '83955664455', '1980-03-03', 'PACIENTE');
-
-
-INSERT INTO CONSULTAS(DATA_CONSULTA, PROFISSIONAL, ESPECIALIDADE, DESCRICAO, ID_USUARIO)
-VALUES ('2024-10-28', 'Dr João','Cardiologista', 'Exemplo de descricao', 3),
-('2024-10-29', 'Dra. Maria', 'Nutricionista', 'Exemplo descricao', 3);
